670c7f2232d64cd1784ed4121cf11e757b54433a
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-08-26 17:37:33 +0300

    Exception in salary

--- main.java
+++ main.java
@@ -1,8 +1,8 @@
 public class main {
     public static void main(String[] args) {
         var employee = new Employee();
-        employee.setBaseSalary(20);
-        employee.setHourlyRate(0);
+        employee.setBaseSalary(-1);
+        employee.setHourlyRate(4);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
