c55c0b5a199fd862bd61263776fdc160eb884bb5
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-08-27 11:52:56 +0300

    constructor

--- main.java
+++ main.java
@@ -1,11 +1,17 @@
 public class main {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(-1);
-        employee.setHourlyRate(4);
+        var employee = new Employee(50_000, 20);
+//        employee.setBaseSalary(-1);
+//        employee.setHourlyRate(4);
+//        System.out.println(employee.baseSalary);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
+
+//    public static void main(String[] args){
+//        var browser = new Browser();
+//        browser
+//    }
 
 
 }
