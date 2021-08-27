6973c16bb03e7f044d1de45db604687fe6e3e671
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-08-27 11:53:43 +0300

    constructor

--- Employee.java
+++ Employee.java
@@ -1,16 +1,20 @@
 public class Employee {
     private int baseSalary;
     private int hourlyRate;
+    public Employee(int baseSalary, int hourlyRate ){
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+    }
 
     public int calculateWage(int extraHours) {
         return baseSalary + (getHourlyRate() * extraHours);
     }
 
-    public void setBaseSalary(int baseSalary) {
+    private void setBaseSalary(int baseSalary) {
         if (baseSalary <= 0)
             throw new IllegalArgumentException("Sorry! Salary cannot be 0 or less");
     }
-    public int getBaseSalary(){
+    private  int getBaseSalary() {
         return baseSalary;
     }
 
@@ -18,7 +22,7 @@ public class Employee {
         return hourlyRate;
     }
 
-    public void setHourlyRate(int hourlyRate) {
+    private void setHourlyRate(int hourlyRate) {
         if (hourlyRate <=0)
             throw new IllegalArgumentException("HourlyRate cannot be 0 or -ve");
         this.hourlyRate = hourlyRate;
