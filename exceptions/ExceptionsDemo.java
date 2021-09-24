57c6b96e71cbde6881c46414603453f511035382
Erastuskanyatta <erastusmuriithi146@gmail.com>
2021-09-24 16:31:02 +0300

    try with resources statement

--- exceptions/ExceptionsDemo.java
+++ exceptions/ExceptionsDemo.java
@@ -1,21 +1,17 @@
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 public  class ExceptionsDemo extends Main {
     public static void show(){
 //        sayHello(null);
-        try {
-            var reader = new FileReader("file.text") ;
+        try (
+                var reader = new FileReader("file.text");
+                var writer = new FileWriter("writting some staff");
+        ){   // try with resources statement
             var value = reader.read();
-            System.out.println("file Opened");
-        } catch (FileNotFoundException exception){ // only this catch will be executed
-            System.out.println("no such file");
-        }
-        catch (IOException exeption){
-            System.out.println("cannot read data"); // this catch will be ignored coz it has already been caugt above(poly)
+        }catch (IOException exeption){
+            System.out.println("opps! that error"); // this catch will be ignored coz it has already been caugt above(poly)
         }
     }
-//    public static void sayHello(String name) {
-//        System.out.println(name.toUpperCase());
-//    }
 }
